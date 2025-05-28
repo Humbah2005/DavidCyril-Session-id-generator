68b5d59409e62a369d277e0ff9ff6197453a9da6
Humbah2005 <tanakapunungwe21@gmail.com>
2025-05-28 23:54:21 +0200

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,5 +1,5 @@
 const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+pastebin = new PastebinAPI('mKoIZabR0uHqg1PCZl-Kc7M9bzSYnlo6')
 const {makeid} = require('./id');
 const express = require('express');
 const fs = require('fs');
